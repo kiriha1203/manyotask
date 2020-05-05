df7c63e09c1061577b658cb2d569731f4288fd63
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-05-05 13:06:45 +0900

    Fix 後置ifに変更　コメント補足追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -24,9 +24,8 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    if params[:user][:password].blank?
-      params[:user].delete("password")
-    end
+    # editのパスワード無記入時元のまま登録させる（バリデーションエラーを発生させない）
+    params[:user].delete("password") if params[:user][:password].blank?
     if @user.update(user_params)
       redirect_to admin_user_url(@user), success: "ユーザー「#{@user.name}」を更新しました。"
     else
